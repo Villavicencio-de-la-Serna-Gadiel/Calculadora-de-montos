3674fc8739f7e0e897a969f1395994905b9f5bc1
Villavicencio-de-la-Serna-Gadiel <gadiel.villavicencio.delaserna@gmail.com>
2026-07-22 23:07:54 -0500

    Code organization

--- analisis.py
+++ analisis.py
@@ -84,10 +84,10 @@ def analisis(tab2):
                       key = "quitar_filtros")
     with tab2:
         if "letra" in st.session_state:
-            st.header("Análisis")
             progresion_monto = st.session_state.letra.progresion_monto()
             logica_filtros()
             con_filtro = en_session_state("con_filtro", False)
+             st.header("Análisis")
             if con_filtro:
                 filtrado = st.session_state.filtrado
                 mostrar_graficos(filtrado)
