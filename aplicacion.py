4b92798d56991561f81ee53ca14e69e203ec2268
Villavicencio-de-la-Serna-Gadiel <gadiel.villavicencio.delaserna@gmail.com>
2026-07-25 13:34:13 -0500

    Improve the title

--- aplicacion.py
+++ aplicacion.py
@@ -5,7 +5,7 @@ import analisis as an
 import logica as l
 import datos as d
 def configuracion():
-    st.set_page_config(page_title = "Capiter|Calculadora de intereces",
+    st.set_page_config(page_title = "Aqevut | Calculadora de intereces",
                        page_icon = "📊")
 def titulos():
     st.title("Calculadora de montos")
