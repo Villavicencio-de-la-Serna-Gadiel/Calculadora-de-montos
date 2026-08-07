4976f0dea35e7d375fbf0872720d067e1645ea08
Villavicencio-de-la-Serna-Gadiel <gadiel.villavicencio.delaserna@gmail.com>
2026-08-07 17:05:03 -0500

    Fix navigation page paths in aplicacion.py
    Updated page paths in navigation to use forward slashes.

--- aplicacion.py
+++ aplicacion.py
@@ -3,11 +3,11 @@ def configuracion():
     st.set_page_config(page_title = "Aqevut|Calculadora de intereses",
                        page_icon = "📊")
 configuracion()
-paginas = st.navigation(pages = [st.Page(page = "inicio\\inicio.py",
+paginas = st.navigation(pages = [st.Page(page = "inicio/inicio.py",
                                          title = "Inicio"),
-                                 st.Page(page = "calculadora_normal\\calculadora_normal.py",
+                                 st.Page(page = "calculadora_normal/calculadora_normal.py",
                                          title = "Calculadora normal"),
-                                 st.Page(page = "calculadora_intereses\\calculadora_intereses.py",
+                                 st.Page(page = "calculadora_intereses/calculadora_intereses.py",
                                          title = "Calculadora de intereses")],
                         position = "top")
 paginas.run()
