ac3863eca99a5ab80829947e629bd9f18ba52ea3
Villavicencio-de-la-Serna-Gadiel <gadiel.villavicencio.delaserna@gmail.com>
2026-08-07 16:59:21 -0500

    Delete funciones_compartidas.py

--- funciones_compartidas.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-def entrada_numero(texto1, texto2):
-    return st.number_input(texto1,
-                           min_value=1.00,
-                           step=1.00,
-                           key=texto2)
